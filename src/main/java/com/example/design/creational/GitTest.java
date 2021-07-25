b09b7cb73b4e28d70f42464e47b51c63c6418e26
gxdbox <gxd683@126.com>
2021-07-25 19:35:19 +0800

    test

--- src/main/java/com/example/design/creational/GitTest.java
+++ src/main/java/com/example/design/creational/GitTest.java
@@ -3,5 +3,6 @@ package com.example.design.creational;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("dddd");
+        String test = "test";
     }
 }
