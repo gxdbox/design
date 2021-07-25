c89ec354083e1fbaa38c519a45a5a1bf578cfa0c
gxdbox <gxd683@126.com>
2021-07-25 19:53:12 +0800

    修复

--- src/main/java/com/example/design/creational/GitTest.java
+++ src/main/java/com/example/design/creational/GitTest.java
@@ -4,10 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("dddd");
         String test = "test";
-<<<<<<< HEAD
-        String test2 = "tests";
-        String test3 = "test3";
-=======
->>>>>>> parent of 62090e5... test2
+
     }
 }
