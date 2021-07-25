5bde5e06d6f9b6b3b7c62a8de5d83e2786b1aad5
gxdbox <gxd683@126.com>
2021-07-25 19:50:05 +0800

    no message

--- src/main/java/com/example/design/creational/GitTest.java
+++ src/main/java/com/example/design/creational/GitTest.java
@@ -4,7 +4,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("dddd");
         String test = "test";
+<<<<<<< HEAD
         String test2 = "tests";
         String test3 = "test3";
+=======
+>>>>>>> parent of 62090e5... test2
     }
 }
